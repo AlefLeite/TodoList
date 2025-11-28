c117696b3c8c020adcdaae082e9c1763db8a05a5
Alef Perez Leite <alefperezleite@Alefs-MacBook-Air.local>
2025-11-28 09:04:26 -0300

    Adicionado biblioteca `json-server` e comando para iniciar

--- src/hooks/tasks/index.ts
+++ src/hooks/tasks/index.ts
@@ -1,27 +1,27 @@
-import { useState } from "react"
+import { useState } from "react";
 
 interface Tasks {
-    id: number,
-    nome: string,
-    repete: number
+  id: number;
+  nome: string;
+  repete: number;
 }
-
 
 const useTasks = () => {
-    const [tasks, setTasks] = useState<Tasks[] | []>([]);
+  const [tasks, setTasks] = useState<Tasks[] | []>([]);
 
-    const listarTasks = async () => {
-        const lista = await fetch("http://localhost:3001/atividades")
-            .then((res) => res.json())
+  const listarTasks = async () => {
+    const lista = await fetch("http://localhost:3001/atividades").then((res) =>
+      res.json()
+    );
 
-        setTasks(lista);
-    }
+    setTasks(lista);
+  };
 
-    return {
-        tasks,
-        setTasks,
-        listarTasks
-    }
-}
+  return {
+    tasks,
+    setTasks,
+    listarTasks,
+  };
+};
 
 export default useTasks;
